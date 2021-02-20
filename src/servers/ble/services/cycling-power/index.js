180b1eee551aea9149834a424edd4d0d03d455db
Dan Wasser <djwasser@users.noreply.github.com>
2021-02-20 12:00:46 -0500

    Update index.js

--- src/servers/ble/services/cycling-power/index.js
+++ src/servers/ble/services/cycling-power/index.js
@@ -7,29 +7,20 @@ import {SensorLocationCharacteristic} from './characteristics/sensor-location';
  * Bluetooth LE GATT Cycling Power Service implementation.
  */
 export class CyclingPowerService extends PrimaryService {
-  /**
-   * Create a CyclingPowerService instance.
-   */
-  constructor() {
-    super({
-      uuid: '1818',
-      characteristics: [
-        new CyclingPowerMeasurementCharacteristic(),
-        new CyclingPowerFeatureCharacteristic(),
-        new SensorLocationCharacteristic(),
-      ]
-    })
-  }
-
-  /**
-   * Notify subscriber (e.g. Zwift) of new Cycling Power Measurement.
-   * @param {object} measurement - new cycling power measurement.
-   * @param {number} measurement.power - current power (watts)
-	 * @param {object} [measurement.crank] - last crank event.
-   * @param {number} measurement.crank.revolutions - revolution count at last crank event.
-   * @param {number} measurement.crank.timestamp - timestamp at last crank event.
-   */
-  updateMeasurement(measurement) {
-    this.characteristics[0].updateMeasurement(measurement)
-  }
+	constructor() {
+		let powerMeasurement = new CyclingPowerMeasurementCharacteristic();
+		super({
+			uuid: '1818',
+			characteristics: [
+				new CyclingPowerMeasurementCharacteristic(),
+				new CyclingPowerFeatureCharacteristic(),
+				new SensorLocationCharacteristic(),
+			]
+		});
+		this.powerMeasurement = powerMeasurement;
+	}
+	notify(event) {
+		this.powerMeasurement.notify(event);
+		return this.RESULT_SUCCESS;
+	};
 }
