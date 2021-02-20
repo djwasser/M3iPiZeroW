d0d2ef27f6607b660fbf04b54be0d8e26669d090
Dan Wasser <djwasser@users.noreply.github.com>
2021-02-20 12:18:18 -0500

    Update index.js

--- src/servers/ble/services/fitness-machine/index.js
+++ src/servers/ble/services/fitness-machine/index.js
@@ -11,12 +11,15 @@ export class FitnessMachineService extends PrimaryService {
    * Create a FitnessMachineService instance.
    */
   constructor() {
+    let indoorBikeData = new IndoorBikeDataCharacteristic();
+    let fitnessMachineStatus = new FitnessMachineStatusCharacteristic()
+    
     super({
       uuid: '1826',
       characteristics: [
-        new IndoorBikeDataCharacteristic(),
+        indoorBikeData,
         new StaticReadCharacteristic('2ACC', 'Fitness Machine Feature', [0x02, 0x44, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]), //rpm, power, hr
-        new FitnessMachineStatusCharacteristic()
+        fitnessMachineStatus
       ]
     });
     this.indoorBikeData = indoorBikeData;
