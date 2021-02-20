2d45a067952d214181c319d6236077a3b17358e4
Dan Wasser <djwasser@users.noreply.github.com>
2021-02-20 13:26:25 -0500

    Update index.js

--- src/servers/ble/index.js
+++ src/servers/ble/index.js
@@ -1,6 +1,6 @@
-// This likely wiil take place of keiserBLE from keiser2Zwift repo
+// This likely will take place of keiserBLE from keiser2Zwift repo
 import {CyclingPowerService} from './services/cycling-power'
-import {CyclingSpeedAndCadenceService} from './services/cycling-speed-and-cadence'
+// import {CyclingSpeedAndCadenceService} from './services/cycling-speed-and-cadence'
 import {FitnessMachineService} from './services/fitness-machine'
 import {BleServer} from '../../util/ble-server'
 
@@ -8,7 +8,7 @@ export const DEFAULT_NAME = 'M3iPiZeroW';
 
 /**
  * Handles communication with apps (e.g. Zwift) using the standard Bluetooth Services
- * LE GATT Cycling Power, Cycling Speed and Cadence, & Fitness Maschine
+ * LE GATT Cycling Power, Fitness Maschine
  */
 export class M3iPiZeroW extends BleServer {
   /**
@@ -16,24 +16,18 @@ export class M3iPiZeroW extends BleServer {
    * @param {Bleno} bleno - a Bleno instance.
    */
   constructor(bleno, name=DEFAULT_NAME) {
+    this.cps = new CyclingPowerService();
+    this.fms = new FitnessMachineService();
+
     super(bleno, name, [
-      new CyclingPowerService(),
-      new CyclingSpeedAndCadenceService(),
-      new FtinessMachineService()
+      this.cps,
+      this.fms
     ])
   }
 
-  /**
-   * Notify subscriber (e.g. Zwift) of new Cycling Power Measurement.
-   * @param {object} measurement - new cycling power measurement.
-   * @param {number} measurement.power - current power (watts)
-	 * @param {object} [measurement.crank] - last crank event.
-   * @param {number} measurement.crank.revolutions - revolution count at last crank event.
-   * @param {number} measurement.crank.timestamp - timestamp at last crank event.
-   */
-  updateMeasurement(measurement) {
-    for (let s of this.services) {
-      s.updateMeasurement(measurement)
-    }
-  }
+  // notifiy BLE services
+  notifyFTMS(event) {
+	  this.cps.notify(event);
+	  this.fms.notify(event);
+  };
 }
