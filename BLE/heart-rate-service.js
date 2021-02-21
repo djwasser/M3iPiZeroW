b7fa6d5dcd1085abef36a2b6785de04f11026a3a
Dan Wasser <djwasser@users.noreply.github.com>
2021-02-21 16:58:17 -0500

    Update heart-rate-service.js

--- BLE/heart-rate-service.js
+++ BLE/heart-rate-service.js
@@ -1,1 +1,28 @@
-place holder
+const Bleno = require('@abandonware/bleno');
+
+const HeartRateMeasurementCharacteristic = require('./heart-rate-measurement-characteristic');
+const StaticReadCharacteristic = require('./static-read-characteristic');
+
+class HeartRateService extends Bleno.PrimaryService {
+
+  constructor() {
+    let HeartRateMeasurement = new HeartRateMeasurementCharacteristic();
+    super({
+        uuid: '180D',
+        characteristics: [
+          HeartRateMeasurement,
+          new StaticReadCharacteristic('2A65', 'Cycling Power Feature', [0x00, 0, 0, 0]), // no additional features
+          new StaticReadCharacteristic('2A5D', 'Sensor Location', [13])         // 13 = rear hub
+        ]
+    });
+
+    this.HeartRateMeasurement = HeartRateMeasurement;
+  }
+
+  notify(event) {
+    this.HeartRateMeasurement.notify(event);
+    return this.RESULT_SUCCESS;
+  };
+}
+
+module.exports = HeartRateService;
