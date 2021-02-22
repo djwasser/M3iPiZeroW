7d76483fe7c8e797bf14ead81daf376d56776b90
Dan Wasser <djwasser@users.noreply.github.com>
2021-02-22 15:39:58 -0500

    Update cycling-power-measurement-characteristic.js

--- BLE/cycling-power-measurement-characteristic.js
+++ BLE/cycling-power-measurement-characteristic.js
@@ -1,6 +1,6 @@
 var Bleno = require('@abandonware/bleno');
 var DEBUG = false;
-
+const FLAG_HASCRANKDATA = (1<<5);
 // Spec
 //https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.cycling_power_measurement.xml
 
@@ -13,22 +13,12 @@ class CyclingPowerMeasurementCharacteristic extends  Bleno.Characteristic {
       properties: ['notify'],
       descriptors: [
         new Bleno.Descriptor({
-					uuid: '2901',
-					value: 'Cycling Power Measurement'
-				}),
-        new Bleno.Descriptor({
-          // Client Characteristic Configuration
-          uuid: '2902',
-          value: Buffer.alloc(2)
-        }),
-        new Bleno.Descriptor({
-          // Server Characteristic Configuration
-          uuid: '2903',
-          value: Buffer.alloc(2)
-        })
+		uuid: '2903',
+		value: 'Cycling Power Measurement'
+	})
       ]
     });
-    this._updateValueCallback = null;  
+    this._updateValueCallback = null;
   }
 
   onSubscribe(maxValueSize, updateValueCallback) {
@@ -44,29 +34,25 @@ class CyclingPowerMeasurementCharacteristic extends  Bleno.Characteristic {
   };
 
   notify(event) {
-    if (!('power' in event)) {
-      // ignore events with no power
-      return this.RESULT_SUCCESS;;
+    if (!('power' in event) && !('crankcount' in event) && !('cranktime' in event)) {
+      // ignore events that do not have all of the data expected
+      return this.RESULT_SUCCESS;
     }
   
     if (this._updateValueCallback) {
 		if (DEBUG) console.log("[powerService] Notify");
 		var buffer = new Buffer(8);
-		// flags
-		// 00000001 - 1   - 0x001 - Pedal Power Balance Present
-		// 00000010 - 2   - 0x002 - Pedal Power Balance Reference
-		// 00000100 - 4   - 0x004 - Accumulated Torque Present
-		// 00001000 - 8   - 0x008 - Accumulated Torque Source
-		// 00010000 - 16  - 0x010 - Wheel Revolution Data Present
-		// 00100000 - 32  - 0x020 - Crank Revolution Data Present
-		// 01000000 - 64  - 0x040 - Extreme Force Magnitudes Present
-		// 10000000 - 128 - 0x080 - Extreme Torque Magnitudes Present
-	   
-		buffer.writeUInt16LE(0x0000, 0);  // only power data present
-	   
-		var power = event.power;
-		if (DEBUG) console.log("[powerService] power: " + power);
+	        
+	        let flags = 0;
+	        flags |= FLAG_HASCRANKDATA;
+		buffer.writeUInt16LE(flags, 0);
+	    
+	        const power = event.power;
+	        const revolutions16bit = event.crankcount & 0xffff;
+	        const timestamp16bit = event.cranktime & 0xffff;
 		buffer.writeInt16LE(power, 2);
+	        buffer.writeUInt16LE(revolutions16bit, 4);
+	        buffer.writeUInt16LE(timestamp16bit, 6);
 	  
       this._updateValueCallback(buffer);
     }
