75c0d6a6c7b8bf76cdbb5293db3a4c49bf17bccb
Dan Wasser <djwasser@users.noreply.github.com>
2021-02-22 15:56:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -72,13 +72,14 @@ noble.on('discover', (peripheral) => {
 				console.log(`Bike ${result.ordinalId}: ${result.realTime} ${result.cadence} ${result.power} ${result.gear} ${result.duration}`); 
 				if (result.realTime) {
 					var cranksCurrentEventTime = (cranksLastEventTime + Math.round((60 * 1024)/result.cadence)) % 65535;
+					cranks = cranks + 1;
 					dataToSend = { 
 						rpm: result.cadence,
 						ftmsrpm: result.ftmscadence,
 						power: result.power,
 						hr: result.heartRate,
-						crankcount: = ++cranks,
-						cranktime: = cranksCurrentEventTime;
+						crankcount: cranks,
+						cranktime: cranksCurrentEventTime;
 					};
 					cranks = cranks % 65535;
 					cranksLastEventTime = cranksCurrentEventTime;
