49a520103f10e8fd46753162730e1a7110a1f3f7
Dan Wasser <djwasser@users.noreply.github.com>
2022-01-17 16:01:42 -0500

    Modify properties, remove 2902

--- BLE/fitness-machine-status-characteristic.js
+++ BLE/fitness-machine-status-characteristic.js
@@ -28,14 +28,7 @@ class FitnessMachineStatusCharacteristic extends Bleno.Characteristic {
     super({
       uuid: '2ADA',
       value: null,
-      properties: ['notify'],
-      descriptors: [
-        new Bleno.Descriptor({
-          // Client Characteristic Configuration
-          uuid: '2902',
-          value: Buffer.alloc(2)
-        })
-      ]
+      properties: ['notify','read','write', 'writeWithoutResponse'],
     })
     this._updateValueCallback = null
   }
